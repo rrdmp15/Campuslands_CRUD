ecd5ee479784550e0416da2406485f411ae4ea07
rrdmp15 <rdayismar@gmail.com>
2023-06-21 10:28:12 -0500

    feat: :sparkles: Autoload, and creation files
    the files of the tables and the autoload have been created

--- uploads/app.php
+++ uploads/app.php
@@ -12,18 +12,175 @@
     }
 
     function autoload($class) {
-        // cambiar nombres de directories   
-        $directories = [
-            dirname(__DIR__).'/scripts/bill/',
-            dirname(__DIR__).'/scripts/client/',
-            dirname(__DIR__).'/scripts/product/',
-            dirname(__DIR__).'/scripts/seller/',
-            dirname(__DIR__).'/scripts/db/'
-        ];
+
+        //Staff
+
+        $directoriesStaff = array();
+        $directorioStaff = dirname(__DIR__) . '/scripts/staff';
+        $elementosStaff = scandir($directorioStaff);
+        foreach ($elementosStaff as $elemento) {
+            $rutaElemento = $directorioStaff . '/' . $elemento . '/';
+            if (is_dir($rutaElemento) && $elemento !== '.' && $elemento !== '..') {
+                $directoriesStaff[] = $rutaElemento;
+            }
+        }
         
         $classFile = str_replace('\\', '/', $class) . '.php';
     
-        foreach ($directories as $directory) {
+        foreach ($directoriesStaff as $directory) {
+            $file = $directory.$classFile;
+            if (file_exists($file)) {
+                require $file;
+                break;
+            }
+        }
+
+        // areas
+
+        $directoriesAreas = array();
+        $directorioArea = dirname(__DIR__) . '/scripts/staff';
+        $elementosArea = scandir($directorioArea);
+        foreach ($elementosArea as $elemento) {
+            $rutaElemento = $directorioArea . '/' . $elemento . '/';
+            if (is_dir($rutaElemento) && $elemento !== '.' && $elemento !== '..') {
+                $directoriesAreas[] = $rutaElemento;
+            }
+        }
+        
+        $classFile = str_replace('\\', '/', $class) . '.php';
+    
+        foreach ($directoriesAreas as $directory) {
+            $file = $directory.$classFile;
+            if (file_exists($file)) {
+                require $file;
+                break;
+            }
+        }
+
+        // academic areas
+
+        $directoriesAcademicAreas = array();
+        $directorioAcademicAreas = dirname(__DIR__) . '/scripts/staff';
+        $elementosAcademicAreas = scandir($directorioAcademicAreas);
+        foreach ($elementosAcademicAreas as $elemento) {
+            $rutaElemento = $directorioAcademicAreas . '/' . $elemento . '/';
+            if (is_dir($rutaElemento) && $elemento !== '.' && $elemento !== '..') {
+                $directoriesAcademicAreas[] = $rutaElemento;
+            }
+        }
+        
+        $classFile = str_replace('\\', '/', $class) . '.php';
+    
+        foreach ($directoriesAcademicAreas as $directory) {
+            $file = $directory.$classFile;
+            if (file_exists($file)) {
+                require $file;
+                break;
+            }
+        }
+
+        //team educators 
+
+        $directoriesTeamEducators = array();
+        $directorioTeamEducators = dirname(__DIR__) . '/scripts/staff';
+        $elementosTeamEducators = scandir($directorioTeamEducators);
+        foreach ($elementosTeamEducators as $elemento) {
+            $rutaElemento = $directorioTeamEducators . '/' . $elemento . '/';
+            if (is_dir($rutaElemento) && $elemento !== '.' && $elemento !== '..') {
+                $directoriesTeamEducators[] = $rutaElemento;
+            }
+        }
+        
+        $classFile = str_replace('\\', '/', $class) . '.php';
+    
+        foreach ($directoriesTeamEducators as $directory) {
+            $file = $directory.$classFile;
+            if (file_exists($file)) {
+                require $file;
+                break;
+            }
+        }
+
+        //trainers
+        $directoriesTrainers = array();
+        $directorioTrainers = dirname(__DIR__) . '/scripts/staff';
+        $elementosTrainers = scandir($directorioTrainers);
+        foreach ($elementosTrainers as $elemento) {
+            $rutaElemento = $directorioTrainers . '/' . $elemento . '/';
+            if (is_dir($rutaElemento) && $elemento !== '.' && $elemento !== '..') {
+                $directoriesTrainers[] = $rutaElemento;
+            }
+        }
+        
+        $classFile = str_replace('\\', '/', $class) . '.php';
+    
+        foreach ($directoriesTrainers as $directory) {
+            $file = $directory.$classFile;
+            if (file_exists($file)) {
+                require $file;
+                break;
+            }
+        }
+
+        //journey
+
+        $directoriesJourney = array();
+        $directorioJourney = dirname(__DIR__) . '/scripts/staff';
+        $elementosJourney = scandir($directorioJourney);
+        foreach ($elementosJourney as $elemento) {
+            $rutaElemento = $directorioJourney . '/' . $elemento . '/';
+            if (is_dir($rutaElemento) && $elemento !== '.' && $elemento !== '..') {
+                $directoriesJourney[] = $rutaElemento;
+            }
+        }
+        
+        $classFile = str_replace('\\', '/', $class) . '.php';
+    
+        foreach ($directoriesJourney as $directory) {
+            $file = $directory.$classFile;
+            if (file_exists($file)) {
+                require $file;
+                break;
+            }
+        }
+
+        //location
+
+        $directoriesLocation = array();
+        $directorioLocation = dirname(__DIR__) . '/scripts/staff';
+        $elementosLocation = scandir($directorioLocation);
+        foreach ($elementosLocation as $elemento) {
+            $rutaElemento = $directorioLocation . '/' . $elemento . '/';
+            if (is_dir($rutaElemento) && $elemento !== '.' && $elemento !== '..') {
+                $directoriesLocation[] = $rutaElemento;
+            }
+        }
+        
+        $classFile = str_replace('\\', '/', $class) . '.php';
+    
+        foreach ($directoriesLocation as $directory) {
+            $file = $directory.$classFile;
+            if (file_exists($file)) {
+                require $file;
+                break;
+            }
+        }
+
+        //working info
+
+        $directoriesWorkingInfo = array();
+        $directorioWorkingInfo = dirname(__DIR__) . '/scripts/staff';
+        $elementosWorkingInfo = scandir($directorioWorkingInfo);
+        foreach ($elementosWorkingInfo as $elemento) {
+            $rutaElemento = $directorioWorkingInfo . '/' . $elemento . '/';
+            if (is_dir($rutaElemento) && $elemento !== '.' && $elemento !== '..') {
+                $directoriesWorkingInfo[] = $rutaElemento;
+            }
+        }
+        
+        $classFile = str_replace('\\', '/', $class) . '.php';
+    
+        foreach ($directoriesWorkingInfo as $directory) {
             $file = $directory.$classFile;
             if (file_exists($file)) {
                 require $file;
