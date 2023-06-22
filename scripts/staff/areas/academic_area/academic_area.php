c90e582532d902a3c87e15c71a19bf602175c003
rrdmp15 <rdayismar@gmail.com>
2023-06-22 18:12:51 -0500

    feat: :sparkles: CRUD academic_area
    The CRUD operations (GET, POST, DELETE, and UPDATE) for the "academic_area" table were completed.

---
+++ scripts/staff/areas/academic_area/academic_area.php
@@ -0,0 +1,80 @@
+<?php
+class academic_area extends connect{
+    private $queryPost = 'INSERT INTO academic_area(id_area, id_staff, id_position, id_journey) VALUES(:area,:staff,:position,:journey)';
+    private $queryGetAll = 'SELECT * FROM academic_area';
+    private $queryGet = 'SELECT * FROM academic_area WHERE id = ?';
+    private $queryDelete = 'DELETE FROM academic_area WHERE id = ?';
+    private $queryUpdate = 'UPDATE academic_area SET id_area = ?, id_staff = ?, id_position = ?, id_journey = ? WHERE id = ?';
+    private $message;
+    use getInstance;
+    function __construct(private $id_area = 1, public $id_staff = 1, public $id_position = 1, public $id_journey = 1){
+        parent::__construct();
+    }
+
+    public function postAcademic_area(){
+        try {
+            $res = $this->conx->prepare($this->queryPost);
+            $res->bindValue("area", $this->id_area);
+            $res->bindValue("staff", $this->id_staff);
+            $res->bindValue("position", $this->id_position);
+            $res->bindValue("journey", $this->id_journey);
+            $res->execute();
+            $this->message = ["Code"=> 200+$res->rowCount(), "Message"=> "inserted data"];
+        } catch(\PDOException $e) {
+            $this->message = ["Code"=> $e->getCode(), "Message"=> $res->errorInfo()[2]];
+        }finally{
+            print_r($this->message);
+        }
+    }
+
+    public function getAllAcademic_area(){
+        try {
+            $res = $this->conx->prepare($this->queryGetAll);
+            $res->execute();
+            $this->message = ["Code"=> 200, "Message"=> $res->fetchAll(PDO::FETCH_ASSOC)];
+        } catch(\PDOException $e) {
+            $this->message = ["Code"=> $e->getCode(), "Message"=> $res->errorInfo()[2]];
+        }finally{
+            print_r($this->message);
+        }
+    }
+
+    public function getAcademic_area($id){
+        try{
+            $res = $this -> conx -> prepare($this -> queryGet);
+            $res -> execute([$id]);
+            $this -> message = ["Code" => "200", "Message" => $res -> fetch(PDO::FETCH_ASSOC)];
+        }catch(\PDOException $e){
+            $this -> message = ["Code" => $e -> getCode(), "Message" => $res -> errorInfo()[2]];
+        }finally{
+            print_r($this->message);
+        }
+    }
+
+    public function deleteAcademic_area($id){
+        try{
+
+            $res = $this -> conx -> prepare($this->queryDelete);
+            $res -> execute([$id]);
+            $this -> message = ["Code" => 200, "Message" => $res -> fetch(PDO::FETCH_ASSOC)];
+
+        }catch(\PDOException $e){
+            $this -> message = ["Code" => $e -> getCode(), "Message" => $res -> errorInfo()[2]];
+        }finally{
+            print_r($this->message);
+        }
+    }
+
+    public function updateAcademic_area( $id_area, $id_staff, $id_position, $id_journey, $id){
+        try{
+          $res = $this->conx->prepare($this->queryUpdate);
+            $res->execute([$id_area, $id_staff, $id_position, $id_journey, $id]);
+            $this->message = ["Code"=> 200+$res->rowCount(), "Message"=> "inserted data"];
+        } catch(\PDOException $e) {
+            $this->message = ["Code"=> $e->getCode(), "Message"=> $res->errorInfo()[2]];
+        }finally{
+            print_r($this->message);
+        }
+    }
+}
+?>
