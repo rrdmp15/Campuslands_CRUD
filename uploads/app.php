147d5287289c1e4e1bf9e215075a98146b8a21c7
rrdmp15 <rdayismar@gmail.com>
2023-06-23 09:06:15 -0500

    feat: :sparkles: Htaccess
    The `.htaccess` file was configured and tested for proper functioning.

--- uploads/app.php
+++ uploads/app.php
@@ -1,10 +1,17 @@
 <?php
     require_once '../vendor/autoload.php';
 
+    $router = new \Bramus\Router\Router();
+
+    $router->get('/campus', function() {
+        echo 'Hola Mundo';
+    });
+
+    $router->run();
+
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
-    
 
-    \App\staff::getInstance(json_decode(file_get_contents("php://input"), true))->deleteStaff("13");
 ?>
 <!-- \\\ -->
+<!-- // \App\staff::getInstance(json_decode(file_get_contents("php://input"), true))->deleteStaff("13"); -->
